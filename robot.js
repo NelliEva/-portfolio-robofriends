c7aad066908ab5278d41e66458cd8c5ee21d56e0
NelliEva <55017487+NelliEva@users.noreply.github.com>
2020-05-27 09:10:22 +0200

    Add files via upload

---
+++ robot.js
@@ -0,0 +1,59 @@
+export const robot = [
+{
+	id: 1,
+	name: "Leanna Small",
+	username:  "Small",
+	email: "small@gmail.com"
+},
+{
+	id: 2,
+	name: "Ron Sweet",
+	username:  "Sweet",
+	email: "sweet@gmail.com"
+},
+{
+	id: 3,
+	name: "Simon Cool",
+	username:  "Cool",
+	email: "cool@gmail.com"
+},{
+	id: 4,
+	name: "John Great",
+	username:  "Great",
+	email: "great@gmail.com"
+},{
+	id: 5,
+	name: "Elly Doe",
+	username:  "Doe",
+	email: "doe@gmail.com"
+},
+{
+	id: 6,
+	name: "Bella Good",
+	username:  "Good",
+	email: "good@gmail.com"
+},
+{
+	id: 7,
+	name: "Henry Heart",
+	username:  "Heart",
+	email: "heart@gmail.com"
+},
+{
+	id: 8,
+	name: "Sara Snow",
+	username:  "Snow",
+	email: "snow@gmail.com"
+},{
+	id: 9,
+	name: "Jonny Deep",
+	username:  "Deep",
+	email: "deep@gmail.com"
+},{
+	id: 10,
+	name: "Connor Walsh",
+	username:  "Walsh",
+	email: "walsh@gmail.com"
+},
+
+]
